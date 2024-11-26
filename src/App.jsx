327c8371d936fb39553354df22fb046eb13bedf6
ryoya <sateraito3696@gmail.com>
2024-11-26 10:11:16 +0900

    消したり増やしたり

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 // import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
-
-import { AppBar } from "@mui/material";
-
 function App() {
 
   return (
     <main className="container">
-      <AppBar position="static">
-        
-      </AppBar>
     </main>
   );
 }
