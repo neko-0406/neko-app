936472b8cc3b895c13bbd501f6f76ed3dbc329cd
ryoya <sateraito3696@gmail.com>
2024-11-27 12:55:11 +0900

    add TreeView

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@
 
 import "./App.css";
 import Header from "./components/Header";
+import TreeView from "./components/TreeView";
 function App() {
 
   return (
-      <Header></Header>
+    <div className="root">
+      {/* 上部メニューバー */}
+      <Header/>
+
+      {/* 中央作業場 */}
+      <div className="main">
+        <TreeView />
+      </div>
+    </div>
   );
 }
 
